4063fb34705f68251f76dd9621c46811ce3e2ae4
Oleg <kerki2000@gmail.com>
2019-11-05 19:55:02 -0500

    exam

---
+++ Quiz1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             DisplayPar(5);
+
+            Sum(862);
+            Console.WriteLine(CalculateFactorial(5));
+            Console.ReadKey();
+        }
+        public static void DisplayPar(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write("*");
+                    
+                }
+
+                Console.WriteLine("\n");
+            }
+            
+        }
+                
+
+          public static void Sum(int n)
+        {
+            int sum = 0;
+              while (n > 0)
+            {
+                sum += n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine("Sum of digits of the number is {0}",sum);
+            Console.ReadKey();
+        }
+
+        public static int CalculateFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * CalculateFactorial(n - 1);
+
+            
+        }
+    }
+}
